10b99a551d6864d5163489c033b9e8dd375d83d6
Johnner <jhnner@gmail.com>
2013-11-04 17:15:46 +0400

    add tests

--- test/jirakeycheck_test.py
+++ test/jirakeycheck_test.py
@@ -25,7 +25,7 @@ class MockUI():
     sending the message on screen.
     """
     def warn(self, msg):
-        print msg
+        return msg
 
 
 class TestParsedThreadGrab(unittest.TestCase):
@@ -37,41 +37,114 @@ class TestParsedThreadGrab(unittest.TestCase):
         self.repo = MockRepo()
         self.ui = MockUI()
 
-    def test_check_msg_call(self):
+    def test_correct_commit_msg(self):
+        """
+        When message fits pattern and jira project exists
+        script should pass successfully
+        and return FALSE (yep, that's hg invert logic )
+        """
+        jk.JIRA_PROJECTS = ['MYPRJ']
+        r = self.commit('MYPRJ-1 - test')
+        self.assertEqual(r, self.OK)
+
+        r = self.commit('MYPRJ-1 - test')
+        self.assertEqual(r, self.OK)
+
+        #message can be even empty but with project prefix
+        r = self.commit('MYPRJ-1 - ')
+        self.assertEqual(r, self.OK)
+
+    def test_no_jira_prj(self):
+        """
+        When message fits pattern but jira project doesn't exist
+        script should fail
+        """
+        jk.JIRA_PROJECTS = ['MYPRJ']
+        r = self.commit('NOPROJ-1 - test')
+        self.assertEqual(r, self.BAD_COMMIT)
+
+    def test_bad_commit_msg(self):
+        """
+         Test all sorts of messages that not pass commit msg pattern:
+         <PROJECT-111 - ><commit message>
+        """
+        jk.JIRA_PROJECTS = ['PRJ']
+
+        r = self.commit('plain commit msg')
+        self.assertEqual(r, self.BAD_COMMIT)
+
+        r = self.commit('PRJ-123 commit msg')
+        self.assertEqual(r, self.BAD_COMMIT)
+
+        r = self.commit('PRJ- - commit msg')
+        self.assertEqual(r, self.BAD_COMMIT)
+
+        r = self.commit('XXX-123 - commit msg')
+        self.assertEqual(r, self.BAD_COMMIT)
+
+        r = self.commit('PRJ--123 - commit msg')
+        self.assertEqual(r, self.BAD_COMMIT)
+
+    def test_empty_msg(self):
+        """
+         Message can't be completely empty, it should be with
+         jira project name prefix
+        """
+        jk.JIRA_PROJECTS = ['TEST', 'PRJ']
+        r = self.commit('')
+        self.assertEqual(r, self.BAD_COMMIT)
+
+    @patch('jirakeycheck.checkMessage')
+    def test_check_msg_call(self, test_checkMsg):
         """
         Test that checkMessage has been called
         """
-        jk.checkMessage = MagicMock(return_value=True)
+        test_checkMsg.return_value = True
         jk.checkCommitMessage(ui=self.ui, repo=self.repo)
         self.assertTrue(jk.checkMessage.called)
 
-    def test_check_msg_ok(self):
+    @patch('jirakeycheck.checkMessage')
+    def test_check_msg_ok(self, test_checkMsg):
         """
         Test when checkMessage returns True the script
         finished with success
         """
-        jk.checkMessage = MagicMock(return_value=True)
+        test_checkMsg.return_value = True
         res = jk.checkCommitMessage(ui=self.ui, repo=self.repo)
         self.assertEqual(res, self.OK)
 
-    def test_check_msg_fail(self):
+    @patch('jirakeycheck.checkMessage')
+    def test_check_msg_fail(self, test_checkMsg):
         """
          Test when checkMessage is not passed (returned False)
          the whole script fails and block commit
         """
-        jk.checkMessage = MagicMock(return_value=False)
+        test_checkMsg.return_value = False
         res = jk.checkCommitMessage(ui=self.ui, repo=self.repo)
         self.assertEqual(res, self.BAD_COMMIT)
 
-    def test_fail_user_message(self):
+    @patch('jirakeycheck.checkMessage')
+    def test_fail_user_message(self, test_checkMsg):
         """
         When script fails and blocks commit, the user
         must see output info message.
         """
         with patch.object(MockUI, 'warn') as self.ui.warn:
-            jk.checkMessage = MagicMock(return_value=False)
+            #jk.checkMessage = MagicMock(return_value=False)
+            test_checkMsg.return_value = False
             jk.checkCommitMessage(ui=self.ui, repo=self.repo)
             self.assertTrue(self.ui.warn.called)
+
+    def commit(self, msg):
+        """
+         Emulate commit hook and return hook result:
+         False - everything is ok, hook passed
+         True - commit didn't pass, hook failed
+        """
+        self.repo.description = MagicMock(return_value=msg)
+        hook_result = jk.checkCommitMessage(ui=self.ui, repo=self.repo)
+        return hook_result
+
 
 if __name__ == '__main__':
     unittest.main()
